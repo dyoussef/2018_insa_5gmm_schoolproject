d4db5905204a198778b8903038e97186526ab7ab
nguyen0508 <nguyenvannguyen0508@gmail.com>
2018-11-15 15:30:04 +0100

    change ROI of PMS and PAN

---
+++ Extract_PMS.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+# Import the otb applications package
+import otbApplication
+
+# The following line creates an instance of the ExtractROI application
+ExtractROI = otbApplication.Registry.CreateApplication("ExtractROI")
+url = "/home/nguyen/dumasl//Pleiades_Primary_Product_Pan-sharpened/FCGC600031034/IMG_PHR1A_PMS-N_001/IMG_PHR1A_PMS-N_201202250025599_SEN_PRG_FC_5844-001_R1C1.JP2"
+# The following lines set all the application parameters:
+ExtractROI.SetParameterString("in", url)
+
+ExtractROI.SetParameterString("mode","extent")
+ExtractROI.SetParameterString("mode.extent.unit","pxl")
+
+ExtractROI.SetParameterFloat("mode.extent.ulx", 150)
+
+ExtractROI.SetParameterFloat("mode.extent.uly", 150)
+
+ExtractROI.SetParameterFloat("mode.extent.lrx", 649)
+
+ExtractROI.SetParameterFloat("mode.extent.lry", 649)
+
+ExtractROI.SetParameterString("out", "ExtractROI_PMS.tif")
+
+# The following line execute the application
+ExtractROI.ExecuteAndWriteOutput()
--- README.md
+++
@@ -1,2 +0,0 @@
-# 2018_insa_5gmm_schoolproject
-Simplification d'un modèle 3D à partir de classifications en géométrie senseur
